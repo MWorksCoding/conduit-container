69807bde7874585d1477189dce6a3aff28ef8103
MWorksCoding <mail.org@posteo.org>
2025-12-02 09:03:05 +0100

    updated docker-compose, dockerfile and requirements.txt

--- conduit-backend-master/conduit/settings.py
+++ conduit-backend-master/conduit/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'conduit.urls'
@@ -128,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
 
 CORS_ORIGIN_WHITELIST = (
     '0.0.0.0:4000',
